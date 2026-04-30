c3d04f11180fa97ffad52ab2d41a39a99401bd15
Davidhuynh-tech <s223482424@deakin.edu.au>
2026-04-30 15:51:11 +1000

    clean branch

--- app/src/main/java/com/example/ailearningapp/model/UserAccount.java
+++
@@ -1,8 +0,0 @@
-package com.example.ailearningapp.model;
-
-public class UserAccount {
-    public String username;
-    public String email;
-    public String password;
-    public String phoneNumber;
-}
--- app/src/main/java/com/example/ailearningapp/network/dto/ExplainAnswersRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.ailearningapp.network.dto;
-
-import com.example.ailearningapp.model.SubmittedAnswer;
-
-import java.util.List;
-
-public class ExplainAnswersRequest {
-    public List<SubmittedAnswer> answers;
-}
